179401d43e1512ec1a7bc202d92549a316d26b2e
Rakesh Gaddam <rakesh.gaddam120@gmail.com>
2023-03-14 23:39:28 +0530

    day02 &3

---
+++ src/day02/program02.java
@@ -0,0 +1,35 @@
+package day02;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class program02 {
+
+	public static void main(String[] args) {
+		WebDriver driver;	
+		driver = new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/Register.html");
+		driver.navigate().to("https://demo.automationtesting.in/Register.html");
+		
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().window().minimize();
+		
+		driver.manage().window().fullscreen();
+		
+		
+		driver.navigate().refresh();
+		
+		driver.navigate().back();
+		
+		driver.navigate().forward();
+		
+		driver.close();
+		
+		driver.quit();
+
+
+	}
+
+}
